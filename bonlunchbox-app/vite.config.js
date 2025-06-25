fe74c48e42ee81d97bdbabe111c65d53b0a3db6e
ASM <nano000417@naver.com>
2025-06-25 09:47:32 +0900

    리엑트 본도시락 커밋

---
+++ bonlunchbox-app/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base : '/',
+  plugins: [react()],
+  // 2. server의 proxy 설정
+  // 5173 port -> 8088port에 요청을 보낸다.
+  // Cross-origin 간주하고 이를 차단! CROS
+  server : {
+    proxy:{
+      '/api' : 'http://localhost:8088'
+    }
+  }
+
+
+})
